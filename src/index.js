8398e11b75f0a33141e338001ee9c501fceb9996
Piercarlo <Piercarlo@DESKTOP-MOKKDO9.mshome.net>
2019-05-30 11:01:21 -0300

    Setup project and stack. Added bootstrap. Hello World.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/app.scss';
+        const App = () => (
+        <div>
+            <h1>Hello world!!</h1>
+        </div>
+)
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
