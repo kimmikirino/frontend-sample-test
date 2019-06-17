95aaa46fc4ca719a6021bbd4fc8533c132b932e9
Paula Lopes <pls21@msn.com>
2019-06-17 04:01:36 -0300

    Layout component header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule,
+
+    HeaderComponent
   ]
 })
 export class CoreModule { }
